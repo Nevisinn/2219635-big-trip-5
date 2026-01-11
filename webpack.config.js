5e1bb7847c71046f6983967ee216d1739c990161
Nevisinn <igor.shub93@mail.ru>
2026-01-11 13:34:12 +0400

    Финальные правки

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,49 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+ const HtmlPlugin = require('html-webpack-plugin');
+ const path = require('path');
 
-module.exports = {
-  entry: "./src/main.js",
+ module.exports = {
+   entry: './src/main.js',
+   output: {
+     filename: 'bundle.[contenthash].js',
+     path: path.resolve(__dirname, 'build'),
+     clean: true,
+   },
 
-  output: {
-    filename: "bundle.[contenthash].js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
+ devtool: 'source-map',
+ plugins: [
+   new CopyPlugin({
+     patterns: [
+       {
+         from: 'public',
+         globOptions: {
+           ignore: ['**/index.html'],
+         },
+       },
+     ],
+   }),
+   new HtmlPlugin({
+     template: 'public/index.html',
+   }),
+ ],
 
-  devtool: "source-map",
+ module: {
+   rules: [
+     {
+       test: /\.js$/,
+       exclude: /(node_modules)/,
+       use: {
+         loader: 'babel-loader',
+         options: {
+           presets: ['@babel/preset-env']
+         },
+       },
+     },
+     {
+       test: /\.css$/i,
+       use: ['style-loader', 'css-loader']
+     },
+   ],
+ },
 
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "public"),
-          globOptions: {
-            ignore: ["**/index.html"],
-          },
-        },
-      ],
-    }),
-
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html"),
-    }),
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
-};
+ }
